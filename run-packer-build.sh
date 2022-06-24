bb13e79b8809d7643c5667c14d9bf96d9d1f575e
ilegra <gabrielysantos.informatica@gmail.com>
2022-06-24 17:19:07 -0300

    Update file to run packer command

--- run-packer-build.sh
+++ run-packer-build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
 apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 apt-get update && apt-get install packer
 
