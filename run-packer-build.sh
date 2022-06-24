875876710270a702de1b229b21c7fc4dd44814ef
ilegra <gabrielysantos.informatica@gmail.com>
2022-06-24 17:35:43 -0300

    Update file to run packer command

--- run-packer-build.sh
+++ run-packer-build.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
-apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-apt-get update && apt-get install packer
-
 packer build -var "REPOSITORY=$DOCKERHUB_REPOSITORY" -var "USERNAME=$DOCKERHUB_CREDENTIALS_USR" -var "PASSWORD=$DOCKERHUB_CREDENTIALS_PSW" Get_Calculator_Artifactory/build_image.pkr.hcl
