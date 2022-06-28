69385eb1953e5e210e1311c4316d0135ef4e6ae8
ilegra <gabrielysantos.informatica@gmail.com>
2022-06-28 18:10:40 -0300

    Delete unused file

--- run-packer-build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-packer build -var "REPOSITORY=$DOCKERHUB_REPOSITORY" -var "USERNAME=$DOCKERHUB_CREDENTIALS_USR" -var "PASSWORD=$DOCKERHUB_CREDENTIALS_PSW" Get_Calculator_Artifactory/build_image.pkr.hcl
